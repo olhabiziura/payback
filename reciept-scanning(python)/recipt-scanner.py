269b3b894c8dc5fc121fa58913295dc5a8f3a547
Tsion biruk ephrem <106772183+tsionbiruk@users.noreply.github.com>
2024-06-24 15:34:49 +0200

    recipt-scanner.py
    It will scan the image specified in 'imageFile'. i parsed the relevant content and saved it in the "recipets.json" file. from there I further parsed it with the for loop and get only the relevant things.

--- reciept-scanning(python)/recipt-scanner.py
+++ reciept-scanning(python)/recipt-scanner.py
@@ -2,11 +2,12 @@ import requests
 import json
 
 
-new_data=[]
+amount_description=[]
+currency_total=[]
 
 
 receiptOcrEndpoint = 'https://ocr.asprise.com/api/v1/receipt' # Receipt OCR API endpoint
-imageFile = "receipt.png" # // Modify it to use your own file
+imageFile = "test_recipets.jpg" 
 r = requests.post(receiptOcrEndpoint, data = { \
   'api_key': 'TEST',        # Use 'TEST' for testing purpose \
   'recognizer': 'auto',       # can be 'US', 'CA', 'JP', 'SG' or 'auto' \
@@ -21,19 +22,20 @@ save_file=open("reciepts.json","w")
 json.dump(json_data['receipts'][0],save_file,indent=6)
 save_file.close()
 
-print("all good till here")
-
 
 with open('reciepts.json','r') as file:
   data=json.load(file)
 
-print(data)
-amount= data['items']['amount']
-description=data['items']['description']
 
-new_data.append(dict(amount=amount,
-                        description=description ))
+for dikt in data['items']:
 
+  amount= dikt['amount']
+  description=dikt['description']
 
+  amount_description.append(dict(amount=amount, description=description ))
 
-#print(r.text) # result in JSON
+currency_total.append(data['currency'])
+currency_total.append(data['total'])
+
+print(amount_description) 
+print(currency_total)
